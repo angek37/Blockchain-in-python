a8dce36eb03ac6b74a231bca6086a11c8dcc3622
Miguel Rodríguez <angek37@outlook.com>
2018-02-19 16:40:04 -0600

    Hashea el block

--- Block.py
+++ Block.py
@@ -1,12 +1,21 @@
 import datetime
+import hashlib
 
-class Blockchain:
+class Block:
     hash = ""
     data = ""
     previousHash = ""
     timeStamp = ""
 
+    #Constructor
     def __init__(self, data, previousHash):
         self.data = data
         self.previousHash = previousHash
         self.timeStamp = datetime.datetime.now()
+        self.hash = self.calculateHash()
+        print(self.hash)
+
+    def calculateHash(self):
+        inp = "{}{} {}".format(self.previousHash, self.timeStamp, self.data)
+        calculatedHash = hashlib.sha256(str.encode(inp, "UTF-8")).hexdigest()
+        return calculatedHash
